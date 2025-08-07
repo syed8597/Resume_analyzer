8ae1e76115823fd01130178116da04f14f8072a3
haneef <haneefsyed0000@gmail.com>
2025-08-08 00:13:06 +0500

    commented spacy

--- resumes/resume_parser.py
+++ resumes/resume_parser.py
@@ -1,15 +1,15 @@
 import fitz  # PyMuPDF
 import re
-import spacy
+# import spacy
 import importlib.util
 
 # Check if model is available, if not, download and install
-try:
-    nlp = spacy.load("en_core_web_sm")
-except OSError:
-    from spacy.cli import download
-    download("en_core_web_sm")
-    nlp = spacy.load("en_core_web_sm")
+# try:
+#     nlp = spacy.load("en_core_web_sm")
+# except OSError:
+#     from spacy.cli import download
+#     download("en_core_web_sm")
+#     nlp = spacy.load("en_core_web_sm")
 
 
 from nltk.corpus import stopwords
