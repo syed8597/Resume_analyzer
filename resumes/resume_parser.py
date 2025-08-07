a427a0502ad1664a514723e316371dd7ab71c67d
haneef <haneefsyed0000@gmail.com>
2025-08-07 21:25:15 +0500

    Fixed SpaCy model loading issue

--- resumes/resume_parser.py
+++ resumes/resume_parser.py
@@ -1,13 +1,15 @@
 import fitz  # PyMuPDF
 import re
-
 import spacy
+import importlib.util
 
+# Check if model is available, if not, download and install
 try:
     nlp = spacy.load("en_core_web_sm")
 except OSError:
-    raise RuntimeError("Spacy model not loaded. Make sure en_core_web_sm is in requirements.txt as a .whl.")
-
+    from spacy.cli import download
+    download("en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
 
 from nltk.corpus import stopwords
 import nltk
